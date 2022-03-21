fd959ecdda67a2696275189de4ba8f74cd7ba903
sato-souma <59986133+sato-souma@users.noreply.github.com>
2022-03-22 05:35:00 +0900

    Graph カスタムフック切り出し

--- public/src/components/organisms/Graph/Graph.tsx
+++ public/src/components/organisms/Graph/Graph.tsx
@@ -1,28 +1,10 @@
 import { useRef } from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
-import { useSelector } from 'react-redux'
-import { PrefInfo, Year } from 'public'
-import { prefInfo } from 'types/reduxTypes'
+import { useGraph } from './useGraph'
 
 const Graph: React.VFC<HighchartsReact.Props> = (props) => {
-  const prefInfo = useSelector(PrefInfo)
-  const year = useSelector(Year)
-
-  const options: Highcharts.Options = {
-    title: {
-      text: '総人口推移グラフ',
-    },
-    xAxis: { title: { text: '人口数' }, categories: year },
-    yAxis: { title: { text: '年度' } },
-    series: [],
-  }
-
-  prefInfo.map((val: prefInfo) => {
-    if (!!options.series) {
-      options.series.push({ type: 'line', name: val.prefName, data: val.value })
-    }
-  })
+  const [options] = useGraph()
 
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null)
   return (
--- public/src/components/organisms/Graph/useGraph.tsx
+++ public/src/components/organisms/Graph/useGraph.tsx
@@ -1,1 +1,25 @@
-export function useGraph() {}
+import { useSelector } from 'react-redux'
+import { PrefInfo, Year } from 'public'
+import { prefInfo } from 'types/reduxTypes'
+
+export function useGraph() {
+  const prefInfo = useSelector(PrefInfo)
+  const year = useSelector(Year)
+
+  const options: Highcharts.Options = {
+    title: {
+      text: '総人口推移グラフ',
+    },
+    xAxis: { title: { text: '年' }, categories: year },
+    yAxis: { title: { text: '人口数' } },
+    series: [],
+  }
+
+  prefInfo.map((val: prefInfo) => {
+    if (!!options.series) {
+      options.series.push({ type: 'line', name: val.prefName, data: val.value })
+    }
+  })
+
+  return [options] as const
+}
