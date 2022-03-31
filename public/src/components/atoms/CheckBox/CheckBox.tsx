aa92d397974a37c4a031085395deb62371378269
sato-souma <59986133+sato-souma@users.noreply.github.com>
2022-03-31 19:38:35 +0900

    ラベルをクリックしてチェックボックス操作可能

--- public/src/components/atoms/CheckBox/CheckBox.tsx
+++ public/src/components/atoms/CheckBox/CheckBox.tsx
@@ -4,8 +4,8 @@ import styles from './CheckBox.module.scss'
 const CheckBox: React.VFC<checkBox> = ({ prefCode, prefName, handleClick }) => {
   return (
     <div className={styles.checkBox}>
-      <input type="checkbox" value={prefCode} name={prefName} onChange={(e) => handleClick(e)} className={styles.input} />
-      <label htmlFor={prefName}>{prefName}</label>
+      <input type="checkbox" value={prefCode} name={prefName} id={prefCode as unknown as string} onChange={(e) => handleClick(e)} className={styles.input} />
+      <label htmlFor={prefCode as unknown as string}>{prefName}</label>
     </div>
   )
 }
